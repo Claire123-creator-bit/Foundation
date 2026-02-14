9738026643f8ecfb482994d82aa380da87b50aa4
mbogoclaire <clairembogo110@gmail.com>
2026-02-14 16:25:28 +0300

    initial commit

--- frontend/src/utils/apiConfig.js
+++ frontend/src/utils/apiConfig.js
@@ -1,7 +1,5 @@
 // API Configuration
-// Using deployed backend on Render
-// For local development, change to 'http://localhost:8080'
-
+// Point to deployed backend
 const API_BASE = 'https://foundation-0x4i.onrender.com';
 
 export default API_BASE;
